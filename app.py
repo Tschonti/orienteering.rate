528efbffdd762dca30859a04807b34e457f56114
Tschonti <feketesamu@gmail.com>
2020-03-28 12:28:58 +0100

    28/03/2020 build

--- app.py
+++ app.py
@@ -1007,3 +1007,7 @@ def privacy():
 @app.errorhandler(404)
 def page_not_found(e):
 	return render_template('404.html'), 404
+
+@app.errorhandler(500)
+def servererror(e):
+	return render_template('500.html'), 500
